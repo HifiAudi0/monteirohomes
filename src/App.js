8a72d6c89c75bb2b0658dc54f7171b4b45840d05
HifiAudi0 <77525153+HifiAudi0@users.noreply.github.com>
2023-05-17 12:42:09 -0400

    added social icons and text homepage - services drop down and sub category pages paragraphs added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Route, Routes, Link, NavLink, useNavigate } fr
 import { LinkContainer } from "react-router-bootstrap";
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import Image from 'react-bootstrap/Image';
+
 import './App.css';
 import './textRightToLeft.css';
 import './3dCardImage.css';
@@ -17,6 +19,11 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
 
 function App() {
   return (
@@ -54,6 +61,31 @@ function Home() {
 
       <br />
       {/* <!-- NAV BAR ENDS --> */}
+
+
+      <div className="home-socials">
+
+        {/* transparent bg */}
+        <a className="home-social-icons" href="https://www.facebook.com/profile.php?id=100064311003388s" target="_blank">
+          <img className="rounded-circle glow-effect-links" src="./img/fb.png" width="60px" height="60px" alt="Facebook logo" /><span className="home-social-text" p>facebook.com/profile.php?id=100064311003388</span>
+        </a>
+
+        <a className="home-social-icons" href="tel:905-599-1345">
+          <Image className="rounded-circle glow-effect-links" src="./img/phone.png" width="60px" height="60px" alt="A phone number to reach Saher at." />
+          <span className="home-social-text" p>Phil @ (647) 885-2384</span>
+        </a>
+
+        <a className="home-social-icons" href="mailto:phil@monteirohomes.ca">
+          <img className="rounded-circle glow-effect-links" src="./img/email.png" width="60px" height="60px" alt="An email to each Saher at." />
+          <span className="home-social-text">phil@monteirohomes.ca</span>
+        </a>
+
+
+        <a className="home-social-icons " href="https://www.instagram.com/monteirohomes/" target="_blank">
+          <Image className="rounded-circle glow-effect-links" src="./img/instagram.png" width="60px" height="60px" alt="Instagram logo" /><span className="home-social-text" p>instagram.com/monteirohomes/</span>
+        </a>
+
+      </div>
     </>
   );
 }
@@ -89,9 +121,32 @@ function Links() {
               </Link>
 
 
-              <LinkContainer to={"/Services"}>
-                <a href="/Services" id="" className="nav-link">SERVICES</a>
-              </LinkContainer>
+              {/* <Dropdown>
+                <Dropdown.Toggle id="dropdown-button-dark-example1" className="services-dropdown" variant="secondary">
+                  Services
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu variant="dark">
+                  <Dropdown.Item href="#/action-1" active>
+                    Action
+                  </Dropdown.Item>
+                  <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                  <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                  <Dropdown.Divider />
+                  <Dropdown.Item href="#/action-4">Separated link</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown> */}
+
+              {/* <LinkContainer to={"/Products"}> */}
+              <NavDropdown title="Services" className="nav-fontstyle">
+                <NavDropdown.Item className="nav-fontstyle" eventKey="4.1" href="/services">ALL SERVICES</NavDropdown.Item>
+                <NavDropdown.Item className="nav-fontstyle" eventKey="4.1" href="/kitchen">KITCHEN</NavDropdown.Item>
+                <NavDropdown.Item className="nav-fontstyle" eventKey="4.2" href="/basement">BASEMENT</NavDropdown.Item>
+                <NavDropdown.Item className="nav-fontstyle" eventKey="4.3" href="/garage">GARAGE</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item className="nav-fontstyle" eventKey="4.4" href="/pool">POOLS</NavDropdown.Item>
+              </NavDropdown>
+              {/* </LinkContainer> */}
 
 
               <a href="/Faq" className="nav-link">FAQ</a>
@@ -113,6 +168,7 @@ function Links() {
           <Route path="/" element={<Home />} />
           <Route path="/Home" element={<Home />} />
           <Route path="/Services" element={<Services />} />
+          <Route path="/kitchen" element={<Kitchen />} />
           <Route path="/Faq" element={<Faq />} />
           <Route path="/Gallery" element={<Gallery />} />
           <Route path="/Quote" element={<Quote />} />
@@ -130,9 +186,18 @@ function Services() {
   return (
     <>
 
+      <p>Welcome to our expert home renovation services! As a trusted and experienced contractor, we take great pride in transforming houses into dream homes. Our dedicated team of professionals is committed to delivering exceptional results that exceed your expectations. Whether you're looking to update your kitchen, remodel your bathroom, or renovate your entire living space, we have the expertise and skills to bring your vision to life.<br /><br /> We understand that every home is unique, and we work closely with our clients to create personalized designs that reflect their style and preferences. From concept to completion, we handle every aspect of the renovation process with meticulous attention to detail, ensuring a seamless and stress-free experience for our clients. Our services encompass a wide range of specialties, including flooring, painting, plumbing, electrical work, cabinetry, and more.<br /><br /> With a focus on quality craftsmanship and using premium materials, we strive to enhance the beauty, functionality, and value of your home. We are committed to delivering projects on time and within budget, without compromising on quality. Customer satisfaction is our top priority, and we go the extra mile to ensure that our clients are delighted with the final results. Trust us with your home renovation needs, and let us turn your house into the home of your dreams. Contact us today to discuss your project and schedule a consultation.
+      </p>
 
 
+    </>
+  );
+}
 
+function Kitchen() {
+  return (
+    <>
+      <p>At our home renovation company, we specialize in providing top-notch kitchen remodeling services that will breathe new life into your culinary space. Our team of skilled professionals understands the significance of the kitchen as the heart of your home, and we are dedicated to creating a functional, stylish, and inviting space that meets your unique needs.<br /><br /> Whether you desire a complete kitchen overhaul or a targeted renovation, we offer a wide range of services to suit your requirements. From custom cabinetry and countertop installations to flooring upgrades and appliance replacements, we handle every aspect of your kitchen project with utmost care and attention to detail. Our team works closely with you to understand your vision, offering expert advice and guidance along the way.<br /><br /> We pride ourselves on using high-quality materials and employing innovative design techniques to ensure a stunning end result. With our kitchen renovation services, you can expect seamless project management, timely completion, and exceptional craftsmanship. Transform your kitchen into a space that reflects your style and enhances your daily life by entrusting us with your kitchen renovation project. Contact us today to get started on creating the kitchen of your dreams.</p>
     </>
   );
 }
@@ -180,69 +245,69 @@ function Gallery() {
 function Faq() {
   return (
     <>
-
+      <h3>You have questions?</h3>
+      <h3>We have answers!</h3>
       <Accordion className="accordion">
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel1a-content"
           id="panel1a-header"
         >
-          <Typography className="accordHeading">Is it Free to use?</Typography>
+          <Typography className="accordHeading">How much does it cost to do?....</Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography className="accordPara">
-            Yes, it is free to use.
+            All our work is customized for your individual needs. We will provide you with a free estimate after we have a chance to discuss your project with you.
           </Typography>
         </AccordionDetails>
       </Accordion>
       <Accordion className="accordion">
         <AccordionSummary aria-controls="panel2a-content" id="panel2a-header">
-          <Typography className="accordHeading">How do I get started?</Typography>
+          <Typography className="accordHeading">Why should I choose you?</Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography className="accordPara">
-            Just copy and paste the YouTube&#169;&trade; video URL into the form and click submit.
+
           </Typography>
         </AccordionDetails>
       </Accordion>
       <Accordion className="accordion">
         <AccordionSummary aria-controls="panel2a-content" id="panel2a-header">
-          <Typography className="accordHeading">Is it safe to use?</Typography>
+          <Typography className="accordHeading">What area's do you service?</Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography className="accordPara">
-            Yes, it is safe to use. This service is web-based meaning, no need to download or install anything on your computer.
+            We provide our services all throughtout the GTA and surrounding areas. Get in touch with us today about your home renovation project. We will make things right.
           </Typography>
         </AccordionDetails>
       </Accordion>
       <Accordion className="accordion">
         <AccordionSummary aria-controls="panel2a-content" id="panel2a-header">
-          <Typography className="accordHeading">Does it work with any Youtube&#169;&trade; video?</Typography>
+          <Typography className="accordHeading">?</Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography className="accordPara">
-            Yes it works with almost any Youtube&#169;&trade; video, where an auto generated transcript is available.
-            Not sure if it will work with your video? Just try it out!
+
           </Typography>
         </AccordionDetails>
       </Accordion>
       <Accordion className="accordion">
         <AccordionSummary aria-controls="panel2a-content" id="panel2a-header">
-          <Typography className="accordHeading">Muting lasts for a few seconds or longer, why?</Typography>
+          <Typography className="accordHeading">?</Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography className="accordPara">
-            When a swear word is detected within a sentence, the muting will last until the end of the sentence. Not the start and end of any single word.
+
           </Typography>
         </AccordionDetails>
       </Accordion>
       <Accordion className="accordion">
         <AccordionSummary aria-controls="panel2a-content" id="panel2a-header">
-          <Typography className="accordHeading">What languages are currently supported?</Typography>
+          <Typography className="accordHeading">?</Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography className="accordPara">
-            The languages currently supported area: 🇺🇸 English, 🇪🇸 Spanish, 🇮🇹 Italian and 🇩🇪 German. More languages are coming soon, stay tuned!
+
           </Typography>
         </AccordionDetails>
       </Accordion>
@@ -262,6 +327,30 @@ function Quote() {
 function Contact() {
   return (
     <>
+      <h3>Don't hestaite get in touch with us today!</h3>
+      <div className="socials">
+
+        {/* transparent bg */}
+        <a className="social-icons" href="https://www.facebook.com/profile.php?id=100064311003388s" target="_blank">
+          <img className="rounded-circle glow-effect-links" src="./img/fb.png" width="60px" height="60px" alt="Facebook logo" /><span className="social-text" p>facebook.com/profile.php?id=100064311003388</span>
+        </a><br />
+
+        <a className="social-icons" href="tel:905-599-1345">
+          <Image className="rounded-circle glow-effect-links" src="./img/phone.png" width="60px" height="60px" alt="A phone number to reach Saher at." />
+          <span className="social-text" p>Phil @ (647) 885-2384</span>
+        </a><br />
+
+        <a className="social-icons" href="mailto:phil@monteirohomes.ca">
+          <img className="rounded-circle glow-effect-links" src="./img/email.png" width="60px" height="60px" alt="An email to each Saher at." />
+          <span className="social-text">phil@monteirohomes.ca</span>
+        </a><br />
+
+
+        <a className="social-icons " href="https://www.instagram.com/monteirohomes/" target="_blank">
+          <Image className="rounded-circle glow-effect-links" src="./img/instagram.png" width="60px" height="60px" alt="Instagram logo" /><span className="social-text" p>instagram.com/monteirohomes/</span>
+        </a>
+
+      </div>
     </>
   );
 }
